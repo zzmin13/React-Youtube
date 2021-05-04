da71ffffd065559155364eb98a69501489c6336f
zzmin13 <zzzz1997@naver.com>
2021-05-04 16:43:04 +0900

    Axios로 api 호출하기

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,30 @@
-import './app.css';
+import React, { useEffect, useState } from "react";
+import "./app.css";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 function App() {
-  return (
-    <h1>Hello :)</h1>
-  );
+  const [videos, setVideos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+  const getVideos = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=3&key=${API_KEY}`
+      );
+      setVideos((videos) => response.data.items);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getVideos();
+  }, []); // 빈 배열을 전달하는 이유는 마운트 되었을 때 한번만 비디오를 불러오기 위해서 이다.
+
+  return <h1>{isLoading ? "Loading..." : "We are ready"}</h1>;
 }
 
 export default App;
