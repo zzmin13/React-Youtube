e6a6ae93f29eb6f2665d785905fd909db5660af2
zzmin13 <zzzz1997@naver.com>
2021-05-08 18:47:44 +0900

    video에 mouse 올렸을 때 개선

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -2,6 +2,9 @@ import React, { memo } from "react";
 import { Link } from "react-router-dom";
 
 const Video = memo((props) => {
+  const displayVisible = {
+    display: "",
+  };
   const {
     id,
     videos,
@@ -27,6 +30,10 @@ const Video = memo((props) => {
       <div className="video">
         <div className="video-thumbnail-box">
           <img src={imgURL} alt={title} className="video-thumbnail-img" />
+          <div style={{ display: "" }} className="video-thumbnail-hoverBox">
+            <i class="far fa-play-circle playIcon"></i>
+            <span>지금 재생</span>
+          </div>
         </div>
         <h1 className="video-title">{title}</h1>
         <h6>{channelTitle}</h6>
